435c84018c051a8e5f28f6fab2f70193c8f8d956
Elias Peñalver <95551020+eliasgit32@users.noreply.github.com>
2023-11-02 12:03:16 -0400

    se debe elegir entre distintos tipos de personal para su registro en RegParticipant

--- my-app/src/components/Modals/NewParticipantUCAB.jsx
+++ my-app/src/components/Modals/NewParticipantUCAB.jsx
@@ -5,29 +5,29 @@ import CommunityList from '../lists/CommunityList';
 
 export default function NewParticipantUCAB(props) {
   //Inputs
-  const[cedula, setCedula] = useState('');
-  const[fName, setFName] = useState('');
-  const[sName, setSName] = useState('');
-  const[fLastName, setFLastName] = useState('');
-  const[sLastName, setSLastName] = useState('');
-  const[birthdate, setBirthdate] = useState(new Date().toISOString().substr(0, 10));
-  // const[type, setType] = useState(null);
-  const[community, setCommunity] = useState(0);
-  const[email, setEmail] = useState('');
-  const[telephone, setTelephone] = useState('');
+  const [cedula, setCedula] = useState('');
+  const [fName, setFName] = useState('');
+  const [sName, setSName] = useState('');
+  const [fLastName, setFLastName] = useState('');
+  const [sLastName, setSLastName] = useState('');
+  const [birthdate, setBirthdate] = useState(new Date().toISOString().substr(0, 10));
+  const [type, setType] = useState(props.participant);
+  const [community, setCommunity] = useState(0);
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
   // const[firstTerm, setFirstTerm] = useState(null);
-  const[emailUCAB, setEmailUCAB] = useState('');
+  const [emailUCAB, setEmailUCAB] = useState('');
 
   //Funciones del manejo de inputs
-  const changeCedula = e => setCedula(e.target.value) 
+  const changeCedula = e => setCedula(e.target.value)
   const changeFName = e => setFName(e.target.value)
   const changeSName = e => setSName(e.target.value)
   const changeFLastName = e => setFLastName(e.target.value)
-  const changeSLastName = e => setSLastName(e.target.value) 
+  const changeSLastName = e => setSLastName(e.target.value)
   const changeBirthdate = e => setBirthdate(e.target.value)
-  // const changeType = e => setType(e.target.value)
+  const changeType = e => setType(e.target.value)
   const changeCommunity = e => setCommunity(e.target.value)
-  const changeEmail = e => setEmail(e.target.value) 
+  const changeEmail = e => setEmail(e.target.value)
   const changeTelephone = e => setTelephone(e.target.value)
   // const changeFirstTerm = e => setFirstTerm(e.target.value)
   const changeEmailUCAB = e => setEmailUCAB(e.target.value)
@@ -39,6 +39,7 @@ export default function NewParticipantUCAB(props) {
     setFLastName('');
     setSLastName('');
     setBirthdate(new Date().toISOString().substr(0, 10));
+    setType(props.participant);
     // setCommunity(null);
     setEmail('');
     setTelephone('');
@@ -48,10 +49,11 @@ export default function NewParticipantUCAB(props) {
   const queryClient = useQueryClient();
 
   //Hook de react query para enviar participante
-  const addParticipantMutation =  useMutation({
+  const addParticipantMutation = useMutation({
     mutationFn: createParticipant,
     onSuccess: () => {
       queryClient.invalidateQueries();
+      handleClose();
     }
   })
 
@@ -64,7 +66,7 @@ export default function NewParticipantUCAB(props) {
       firstLastName: fLastName,
       secondLastName: sLastName,
       birthdate: birthdate,
-      type: props.participant,
+      type: type,
       community: community,
       phase: (props.participant === 'Estudiante' ? 'Familiarización' : null),
       email: email,
@@ -73,41 +75,40 @@ export default function NewParticipantUCAB(props) {
     }
 
     addParticipantMutation.mutate(newParticipant);
-    handleClose();
   }
 
-  return(
+  return (
     <div className='modal modal-lg fade' id='modalNewParticipant' aria-hidden='true' tabIndex='-1'>
       <div className='modal-dialog'>
         <div className='modal-content'>
           <div className='modal-header'>
             <h5 className='modal-title'>
-              {props.participant === 'Comunidad' ? 'Registrar Miembro de Comunidad' :  `Registrar ${props.participant}`}
+              {props.participant === 'Comunidad' ? 'Registrar Miembro de Comunidad' : `Registrar ${props.participant}`}
             </h5>
-            <button 
-            type='button' 
-            className='btn-close' 
-            data-bs-dismiss='modal' 
-            aria-label='Close'
-            onClick={handleClose}>  
+            <button
+              type='button'
+              className='btn-close'
+              data-bs-dismiss='modal'
+              aria-label='Close'
+              onClick={handleClose}>
             </button>
           </div>
           <div className='modal-body'>
             {/* Registrar cédula */}
             <div className='mb-3 row'>
-              <label 
-                htmlFor="newParticipantCed" 
+              <label
+                htmlFor="newParticipantCed"
                 className='form-label col-sm-1 col align-self-center'
-                style={{fontSize: '16px'}}
+                style={{ fontSize: '16px' }}
               >
                 Cédula
               </label>
               <div className='col-sm-4'>
-                <input 
+                <input
                   type="text"
                   id='newParticipantCed'
                   className='form-control'
-                  style={{fontSize: '14px'}}
+                  style={{ fontSize: '14px' }}
                   value={cedula}
                   onChange={changeCedula}
                 />
@@ -115,7 +116,7 @@ export default function NewParticipantUCAB(props) {
             </div>
             {/* Nombres */}
             <div className='input-group mb-3'>
-              <span className='input-group-text' part-field='true'> 
+              <span className='input-group-text' part-field='true'>
                 1er y 2do Nombre
               </span>
               <input
@@ -166,13 +167,13 @@ export default function NewParticipantUCAB(props) {
                   id='newUCABbirthdate'
                   value={birthdate}
                   onChange={changeBirthdate}
-                  style={{marginLeft: '-40px'}}
+                  style={{ marginLeft: '-40px' }}
                 />
               </div>
               {/* Escuela/Unidad */}
-              <label 
+              <label
                 htmlFor='newPartCommunity'
-                className={`form-label ${props.community === 'Escuela' ? 'col-sm-1' : 'col-sm-2'} col align-self-center`} 
+                className={`form-label ${props.community === 'Escuela' ? 'col-sm-1' : 'col-sm-2'} col align-self-center`}
                 part-field='true'
               >
                 {props.community}
@@ -185,25 +186,25 @@ export default function NewParticipantUCAB(props) {
                   value={community}
                   onChange={changeCommunity}
                 >
-                  <CommunityList type={props.community}/>
+                  <CommunityList type={props.community} />
                 </select>
               </div>
             </div>
             {/* Correo electrónico */}
             <div className='mb-3 row'>
-              <label 
+              <label
                 htmlFor="newParticipantEmail"
                 className='form-label col-sm-2 col align-self-center'
-                style={{fontSize: '16px'}}
+                style={{ fontSize: '16px' }}
               >
                 Correo
               </label>
               <div className='col-sm-4'>
-                <input 
+                <input
                   type="email"
                   id='newParticipantEmail'
                   className='form-control'
-                  style={{fontSize: '14px'}}
+                  style={{ fontSize: '14px' }}
                   value={email}
                   onChange={changeEmail}
                 />
@@ -211,25 +212,25 @@ export default function NewParticipantUCAB(props) {
               {/* Nro de teléfono */}
               <label
                 htmlFor='newPartTelephone'
-                className={`form-label col-sm-1 col align-self-center`} 
+                className={`form-label col-sm-1 col align-self-center`}
                 part-field='true'
-                // style={{marginLeft: '70px'}}
+              // style={{marginLeft: '70px'}}
               >
                 Teléfono
               </label>
               <div className='col-sm-3'>
-                <input 
+                <input
                   type="text"
                   id='newPartTelephone'
                   className='form-control'
-                  style={{fontSize: '14px'}}
+                  style={{ fontSize: '14px' }}
                   value={telephone}
                   onChange={changeTelephone}
                 />
+              </div>
             </div>
-          </div>
-          {/* Email Institucional */}
-          <div className='mb-2 row' style={{display: (props.participant === 'Comunidad' ? 'none' : 'flex')}}>
+            {/* Email Institucional */}
+            <div className='mb-2 row' style={{ display: (props.participant === 'Comunidad' ? 'none' : 'flex') }}>
               <label
                 htmlFor="newParticipantInstEmail"
                 className='form-label col-sm-2 col align-self-center'
@@ -247,20 +248,46 @@ export default function NewParticipantUCAB(props) {
                   onChange={changeEmailUCAB}
                 />
               </div>
+              {/* Tipo de personal */}
+              <label
+                htmlFor="newPartPersonalType"
+                className='form-label col-sm-2 col align-self-center'
+                style={{ fontSize: '16px', padding: '0',
+                  display: (props.participant !== 'Docente' ? 'none' : 'inline-block') }}
+              >
+                Tipo de Personal
+              </label>
+              <div 
+                className='col-sm-3'
+                style={{display: (props.participant !== 'Docente' ? 'none' : 'block')}}
+              >
+                <select
+                  id="newPartPersonalType"
+                  className='form-select'
+                  part-field='true'
+                  value={type}
+                  onChange={changeType}
+                >
+                  <option value="Docente">Docente</option>
+                  <option value="Administrativo">Administrativo</option>
+                  <option value="Obrero">Obrero</option>
+                </select>
+              </div>
+
             </div>
           </div>
           <div className='modal-footer'>
-          <button 
-            type='button' 
-            className='btn btn-danger' 
-            data-bs-dismiss='modal'
-            onClick={handleClose}>Cancelar</button>
-            <button 
-            type='button' 
-            className='btn btn-success'
-            data-bs-dismiss='modal'
-            onClick={handleSave}>
-              {props.participant === 'Comunidad' ? 'Guardar Miembro de Comunidad' :  `Guardar ${props.participant}`}
+            <button
+              type='button'
+              className='btn btn-danger'
+              data-bs-dismiss='modal'
+              onClick={handleClose}>Cancelar</button>
+            <button
+              type='button'
+              className='btn btn-success'
+              data-bs-dismiss='modal'
+              onClick={handleSave}>
+              {props.participant === 'Comunidad' ? 'Guardar Miembro de Comunidad' : `Guardar ${props.participant}`}
             </button>
           </div>
         </div>
